705f6e28c0149906bc66105c354157296403707d
Jesse Jurman <j.r.jurman@gmail.com>
2018-05-08 23:24:24 -0400

    new tram-one version, fix specs (v7, #25)

--- template/specs/header-spec.js
+++ template/specs/header-spec.js
@@ -1,8 +1,8 @@
-const header = require('../elements/header')
+const header = require('../elements/app-header')
 
 describe('header', () => {
   it('should have the title', () => {
     const page = header()
-    expect(page.toString()).toMatch('%TITLE%')
+    expect(page.outerHTML).toMatch('%TITLE%')
   })
 })
