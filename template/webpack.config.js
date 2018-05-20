3c50c02c80aac76d6cc1296fdd6a0da5e0e1f6cc
jjurman <jjurman@constantcontact.com>
2018-05-19 21:39:39 -0400

    use webpack in favor of budo and browserify

---
+++ template/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+  entry: './main.js',
+  externals: {
+    domino: 'domino'
+  },
+  output: {
+    filename: 'bundle.js',
+    publicPath: './public',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
