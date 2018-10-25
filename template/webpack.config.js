1e89dd4b62ff8db01b0086c6d64da6496adb6bd5
Tina Howard <chh2538@rit.edu>
2018-10-25 00:51:38 -0400

    Add fallback html for when JS is disabled (#47, v13.0.0)

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path')
 const moduleConfig = require('./webpack-module.config')
+const buildPage = require('tram-build-page')
+
+const dehydratedIndex = buildPage({
+  appPath: './main.js',
+  route: '/no-js',
+  indexPath: './public/index.html'
+})
+
 module.exports = {
   entry: './main.js',
   devServer: {
+    before: app => {
+      app.get('/', (req, res) => {
+        res.send(dehydratedIndex)
+      })
+      app.get('/index.html', (req, res) => {
+        res.send(dehydratedIndex)
+      })
+    },
+    after: app => {
+      app.get('*', (req, res) => {
+        res.send(dehydratedIndex)
+      })
+    },
     contentBase: './public',
-    historyApiFallback: true,
     hot: true,
     inline: true,
     host: '0.0.0.0'
