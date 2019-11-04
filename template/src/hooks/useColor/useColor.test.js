47bda4de9c129a762ab57db5fb8e0a5fabf6bd3f
JRJurman <jrjurman@gmail.com>
2019-11-03 20:12:05 -0500

    improve test names

--- template/src/hooks/useColor/useColor.test.js
+++ template/src/hooks/useColor/useColor.test.js
@@ -1,12 +1,12 @@
 import useColor from './useColor'
 
 describe('Color', () => {
-	it('Should return a valid color', () => {
+	it('should return default color', () => {
 		const [color] = useColor()
 		expect(color).toEqual('#e6ef9f')
 	})
 
-	it('Should incremented the color', () => {
+	it('should change value on incrementColor', () => {
 		const [, incrementColor] = useColor()
 		incrementColor()
 		const [color] = useColor()
