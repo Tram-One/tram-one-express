ab840ece52c6ec72aeca5aba21647a566dfc7921
JRJurman <jrjurman@gmail.com>
2019-12-24 23:59:59 -0500

    update for v9 release

--- template/src/hooks/useColor/useColor.js
+++ template/src/hooks/useColor/useColor.js
@@ -1,10 +1,10 @@
-import { useGlobalState } from 'tram-one'
+import { useGlobalObservable } from 'tram-one'
 
 const APP_COLOR = 'APP_COLOR'
 
 export default () => {
-	const [colorIndex, setColorIndex] = useGlobalState(APP_COLOR, 0)
-	const colors = ['#e6ef9f', '#a09fef', '#9fefa1', '#ef9f9f']
+	const [colorIndex, setColorIndex] = useGlobalObservable(APP_COLOR, 0)
+	const colors = ['#a09fef', '#9fefa1', '#ef9f9f']
 	const incrementColor = () => setColorIndex((colorIndex + 1) % colors.length)
 	return [colors[colorIndex], incrementColor]
 }
